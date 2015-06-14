05133089abe157857e28cd2dc981cb74ea2e377d
Jon Mark Searle <jonmarksearle@gmail.com>
2015-06-14 20:01:43 +1000

    Simple Map Exercise

--- workspace/leaflet/js/map.js
+++ workspace/leaflet/js/map.js
@@ -12,33 +12,45 @@ map.addLayer(osmLayer);
 
 
 /* Add data from csv to map */
-d3.csv("/workspace/data/SIS_DATASUPPLY_may_subset.csv", function(d) {
+d3.csv("/leaflet/data/SIS_DATASUPPLY_14.csv", function(d) {
     return {
-        id: d["ACCIDENT_NO"],
-        lng: d[" LONGITUDE"],
-        lat: d[" LATITUDE"],
-        date: d[" ACCIDENT_DATE"],
-        time: d[" ACCIDENT_TIME"],
-        type: d[" ACCIDENT_TYPE"]
+        id:    d["ACCIDENT_NO"],
+        lng:   d["LONGITUDE"],
+        lat:   d["LATITUDE"],
+        date:  d["ACCIDENT_DATE"],
+        time:  d["ACCIDENT_TIME"],
+        type1: d["ACCIDENT_TYPE"],
+        type2: d["DCA_CODE"],		
+		dow:   d["DAY_OF_WEEK"],
+		light: d["LIGHT_CONDITION"],
+		sev:   d["SEVERITY"],
+		rma:   d["RMA"],
+		speed: d["SPEED_ZONE"]		
     };
 }, function(error, rows) {
     geojson1 = [];
     $.each(rows, function(item, entry) {
-        tmp = {
-            "type": "Feature",
-            "properties": {
-                "name": entry.id,
-                "popupContent": "<p><b>" +entry.id+ 
-                    "</b><br>Date: " +entry.date+ 
-                    "<br>Time: " +entry.time+ 
-                    "<br><i>" +entry.type+ "</i></p>"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [entry.lng, entry.lat]
-            }
+			tmp = {
+				"type": "Feature",
+				"properties": {
+					"name": entry.id,
+					"popupContent": 
+						"<p><b>"  +entry.dow+" "+entry.date+" " +entry.time+"</b><br>"+
+						"<br>" +entry.type1+
+						"<br>" +entry.type1+
+						"<br>" +entry.type2+
+						"<br>" +entry.light+
+						"<br>" +entry.sev+
+						"<br>" +entry.rma+
+						"<br>" +entry.speed+
+						"</p>"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [entry.lng, entry.lat]
+				}
         };
-       geojson1.push(tmp); 
+       geojson1.push(tmp);
     })
     L.geoJson(geojson1, {
         onEachFeature: onEachFeature
